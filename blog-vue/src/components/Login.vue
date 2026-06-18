410ec34dd6a7ee6f1c4e8afd240813921ecd67da
LuckyCat <guipuyang@gmail.com>
2026-06-18 15:58:12 +0800

    update

--- blog-vue/src/components/Login.vue
+++ blog-vue/src/components/Login.vue
@@ -1,15 +1,34 @@
 <template>
-  <div>
-      用户名:<input type="text" v-model="loginForm.username" placeholder="请输入用户名"/>
-      <br><br>
-      密码： <input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
-      <br><br>
-      <button v-on:click="login">登录</button>
+  <div class="login_container">
+    	<div class="login_box">
+        <!--头像-->
+        <div class="avatar_box">
+          <img src="/img/avatar.jpg" alt="">
+        </div>
+        
+        <!--登录表单-->
+        <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" class="login_form">
+          <el-form-item prop="username">
+            <el-input v-model="loginForm.username" prefix-icon="User"></el-input>
+          </el-form-item>
+
+          <el-form-item prop="password">
+            <el-input v-model="loginForm.password" prefix-icon="Lock" show-password @keyup.native.enter="login"></el-input>
+          </el-form-item>
+
+          <el-form-item>
+            <div class="btns">
+              <el-button type="primary" @click="login" >登录</el-button>
+              <el-button type="info" @click="resetLoginForm">重置</el-button>
+            </div>
+          </el-form-item>
+
+        </el-form>
+      </div>
   </div>
 </template>
 
 <script>
-
   export default {
     name: 'Login',
     data () {
@@ -18,7 +37,15 @@
           username: '',
           password: ''
         },
-        responseResult: []
+        loginFormRules: {
+					username: [
+						{required: true, message: '请输入用户名', trigger: 'blur'},
+					],
+					password: [
+						{required: true, message: '请输入密码', trigger: 'blur'},
+					]
+				},
+        // responseResult: []
       }
     },
     methods: {
@@ -35,8 +62,69 @@
           })
           .catch(failResponse => {
           })
+      },
+      resetLoginForm () {
+        this.$refs.loginFormRef.resetFields();
       }
     }
+
   }
 </script>
 
+<style scoped>
+	.login_container {
+    box-sizing: border-box;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+		background-color: #2b4b6b;
+	}
+  
+  .login_box {
+		width: 450px;
+		height: 300px;
+		background-color: #fff;
+		border-radius: 3px;
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+	}
+	
+  .login_box .avatar_box {
+		height: 130px;
+		width: 130px;
+		border: 1px solid #eee;
+		border-radius: 50%;
+		padding: 10px;
+		box-shadow: 0 0 10px #ddd;
+		position: absolute;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		background-color: #fff;
+	}
+
+  .login_box .avatar_box img {
+		width: 100%;
+		height: 100%;
+		border-radius: 50%;
+		background-color: #eee;
+	}
+
+  .login_form {
+		position: absolute;
+		bottom: 20px;
+		width: 100%;
+		padding: 0 20px;
+		box-sizing: border-box;
+	}
+
+  .btns {
+    width: 100%;
+		display: flex;
+		justify-content: flex-end;
+	}
+
+</style>
